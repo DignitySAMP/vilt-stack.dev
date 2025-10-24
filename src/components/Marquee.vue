85ab8067870f110d74a642fe94cff674ce8705f3
Davy <davy@datalink.be>
2025-10-24 15:59:45 +0200

    refactor: fix marquee dark:bg-color

--- src/components/Marquee.vue
+++ src/components/Marquee.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="relative flex overflow-hidden select-none bg-white dark:bg-neutral-800">
+  <div class="relative flex overflow-hidden select-none bg-white dark:bg-stone-800">
 		<div class="flex animate-marquee whitespace-nowrap gap-8">
 			<div v-for="i in 4">
 				<slot />
 			</div>
 		</div>
 
-		<div class="pointer-events-none absolute left-0 top-0 bottom-0 w-32 bg-gradient-to-r from-white dark:from-neutral-800 to-transparent"/>
-		<div class="pointer-events-none absolute right-0 top-0 bottom-0 w-32 bg-gradient-to-l from-white dark:from-neutral-800 to-transparent"/>
+		<div class="pointer-events-none absolute left-0 top-0 bottom-0 w-32 bg-linear-to-r from-white dark:from-stone-800 to-transparent"/>
+		<div class="pointer-events-none absolute right-0 top-0 bottom-0 w-32 bg-linear-to-l from-white dark:from-stone-800 to-transparent"/>
   	</div>
 </template>
 
